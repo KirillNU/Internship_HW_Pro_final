8b49b649fb74aa593f454d96b10cf3521250e354
KirillNU <kdbarannik@gmail.com>
2022-12-21 19:43:28 +0300

    Task 1 - 3

--- Task4.py
+++ Task4.py
@@ -6,4 +6,49 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import random
+
+''' случайные координаты точек '''
+x = np.random.rand(1000) * 100  # x - координаты
+y = np.random.rand(1000) ** 0.5  # y - координаты
+
+''' область и подобласть рисования '''
+figure, axes = plt.subplots()
+
+''' цвета точек '''
+axes.scatter(x, y, c='green')
+
+''' цвет фона в областей Figure и Axes '''
+figure.set_facecolor('grey')
+axes.set_facecolor('lightblue')
+
+''' заголовок для Axes '''
+# axes.set_title('График рассеяния')
+plt.title('График рассеяния', color='red', fontsize= 16)  # Команда .title() - и график подписан указанным в скобках именем
+plt.xlabel('Переменная X', color='blue')  # Команда .xlabel() добавит надпись только для оси x
+plt.ylabel('Переменная Y', color='magenta')
+
+''' ширина и высота "Figure" '''
+figure.set_figwidth(7)
+figure.set_figheight(7)
+
+plt.show()
+
+
+# x = np.random.rand(4000) * 100
+# y = np.random.rand(4000) ** 0.5
+#
+# figure, axes = plt.subplots()
+#
+# axes.scatter(x, y, c = [[1, 0, 0, 0.35]], s = 2)
+#
+# figure.set_facecolor('grey')
+# axes.set_facecolor('lightblue')
+#
+# plt.title('График рассеяния', color='red', fontsize= 16)
+# plt.xlabel('Переменная X', color='blue')
+# plt.ylabel('Переменная Y', color='magenta')
+#
+# figure.set_figwidth(8)
+# figure.set_figheight(8)
+#
+# plt.show()
--- Task5.py
+++ Task5.py
@@ -3,8 +3,8 @@
 
 Даны два списка x и y
 
-    x = [-7, -5, 7, 9, 10, 10, 9, -7, 0, 5, 7, 4, 0, 6, 8, 4, 0]
-    y = [0, 2, 2, 5, 5, 1, 0, 0, 2, 6, 6, 2, 1, -3, -3, 1, 1]
+x =[-7, -5, 7, 9, 10, 10, 9, -7, 0, 5, 7, 4, 0, 6, 8, 4, 0]
+y = [0, 2, 2, 5, 5, 1, 0, 0, 2, 6, 6, 2, 1, -3, -3, 1, 1]
 
 Постройте два графика: plot(x,y) и scatter(x,y) на разных координатных плоскостях.
 
@@ -17,4 +17,23 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import random
+
+x = [-7, -5, 7, 9, 10, 10, 9, -7, 0, 5, 7, 4, 0, 6, 8, 4, 0]
+y = [0, 2, 2, 5, 5, 1, 0, 0, 2, 6, 6, 2, 1, -3, -3, 1, 1]
+
+matrix_x = np.array(x)
+matrix_y = np.array(y)
+
+figure, axes = plt.subplots()
+
+axes.scatter(matrix_x, matrix_y, c='red')
+
+plt.xlabel('Переменная X', color='green')
+plt.ylabel('Переменная Y', color='blue')
+
+figure.set_figwidth(8)
+figure.set_figheight(8)
+
+plt.show()
+
+# print(f'Матрица X: {matrix_x} \nМатрица Y: {matrix_y}')
